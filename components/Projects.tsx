1728df351891fcb1113b01800fe39201b0b06694
Hadeeba Taj <hadeebataj@gmail.com>
2022-11-22 15:06:45 +0000

    - view design button

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { Project } from "../typings";
 import { urlFor } from "../sanity";
+import Link from "next/link";
 
 type Props = {
   projects: Project[];
@@ -22,26 +23,30 @@ const Projects = ({ projects }: Props) => {
         {projects.map((project, i) => (
           <div
             key={project._id}
-            className="w-screen flex-shrink-0 snap-center items-center flex xs:flex-col sm:flex-col lg:flex-row space-y-3 justify-center xs:px-8 sm:px-12 lg:px-20 xl:px-20 md:px-10 py-20 md:p-44 h-screen "
+            className="w-screen flex-shrink-0 snap-center items-center flex xs:flex-col sm:flex-col lg:flex-row xs:space-y-1 space-y-3 justify-center xs:px-8 sm:px-12 lg:px-20 xl:px-20 md:px-10 py-20 md:p-44 h-screen "
           >
             <motion.img
               initial={{ y: -300, opacity: 0 }}
               transition={{ duration: 1.2 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true }}
-              //fix image for projects
               src={urlFor(project.image).url()}
               className="xl:h-full xl:w-auto md:pt-8 xs:pt-24 lg:h-80 lg:w-auto md:h-80 md:w-auto sm:h-64 sm:w-auto mx-auto justify-center"
               alt=""
             />
 
-            <div className="space-y-10 md:px-10 px-0 max-w-6xl">
+            <div className="space-y-10 xs:space-y-4 sm:space-y-4 md:px-10 px-0 max-w-6xl">
               <h4 className="md:text-4xl font-semibold text-center sm:text-base">
                 {project.title}
               </h4>
               <p className="md:text-lg text-center sm:text-sm">
                 {project.summary}
               </p>
+              <div className="pt-5 space-x-4 space-y-2 text-center">
+                <Link href={project.linkToBuild}>
+                  <button className="heroButton">View Design</button>
+                </Link>
+              </div>
             </div>
           </div>
         ))}
