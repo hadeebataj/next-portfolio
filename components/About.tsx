53c158ea3cc66f48a67db844418810cb79cce46b
hadeebataj <hadeebataj@gmail.com>
2023-08-29 19:23:42 +0100

    - node version update

--- components/About.tsx
+++ components/About.tsx
@@ -15,24 +15,17 @@ const About = (props: Props) => {
         About
       </h3>
       <motion.img
-        src="/61531844.jpeg"
+        src="/user.png"
         initial={{ x: -200, opacity: 0 }}
         whileInView={{ x: 0, opacity: 1 }}
         transition={{ duration: 1.2 }}
         viewport={{ once: true }}
-        className="-mb-20 mt-40 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
+        className="-mb-20 mt-40 md:mb-0 flex-shrink-0 w-30 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
       />
       <div className="space-y-6 xs:space-y-2 px-0 md:px-10">
         <h4 className="text-4xl font-semibold">Here is little background</h4>
 
         <p className="text-base xs:text-sm">
-          {/* I’m a frontend developer with 2+ years of experience in
-          Typescript/React, HTML/CSS, and I am exploring Node.js, I have been
-          working as a consultant to Microsoft teams on the design and
-          development of team collaboration applications (Microsoft Teams) and
-          cyber security based applications (Azure Active Directory). I’m
-          excited to bring my passion for clean, responsive design to new
-          clients located worldwide. */}
           I started my career in Development in January 2020. I have a total of
           8+ years of experience working for corporate and startups. I have 4+
           years of experience as a Designer and Test Engineer. I have worked as
